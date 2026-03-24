f308be83e1fb820175dab7ab030a971020ebe966
Paxden <paxdenco@gmail.com>
2026-03-24 13:35:13 +0100

    changes 1

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    duration: "2020 - 2025",
+    institution: "Ladoke Akintola University of Technology",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    title: "Senior Secondary Certificate",
+    duration: "2016 - 2019",
+    institution: "ADS Comprehensive College",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "Junior Secondary  Certificate",
+    duration: "2013 - 2016",
+    institution: "Bettercare College",
   }
 ]
